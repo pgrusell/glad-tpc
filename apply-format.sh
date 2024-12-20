72a28676eecefc51bdac13d1130d084059b3b15a
jose-luis-rs <joseluis.rodriguez.sanchez@usc.es>
2024-12-20 22:12:37 +0100

    Checking of clang-format

--- apply-format.sh
+++ apply-format.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find  -type f | grep -e '.(\.C\|\.cpp\|\.cxx\|\.h)$'  | grep -v ^./macros | xargs -L 1 clang-format-15 -i 
+find  -type f | grep -e '.(\.cpp\|\.cxx\|\.h)$'  | grep -v ^./macros | grep -v ^./params | grep -v ^./fitter | xargs -L 1 clang-format-15 -i 
 echo "Use git add -A ; git commit -m \"clang-format all files\" --author=\"white space <whitespace@example.com>\" to commit changes."
